f889eb7a8dbad54e5a47a4a1bdb643991d6d14e2
github username <github email address>
2019-10-17 14:50:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(arr, cb) {
+    let ret = []
+    for(let i = 0; i < arr.length; i++) {
+        ret.push(cb(arr[i]));
+    }
+    return ret;
+}
+
+function reduce(arr, cb, start) {
+    let c = 0;
+    let ret = null
+
+    if (start === undefined){
+        ret = arr[0];
+        c = 1;
+    }
+    else {
+        ret = start;
+    }
+
+    for (let i = c; i < arr.length; i++) {
+        ret = cb(arr[i], ret);
+    }
+    return ret;
+}
